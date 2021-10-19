67afc258b70fe40fcdfbd351d9253601be72a8df
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-20 01:39:48 +0530

    1 file modified

--- src/components/CourseContent.js
+++ src/components/CourseContent.js
@@ -1,11 +1,43 @@
 import React from 'react';
 export default function CourseContent() {
-  const Lecture = () => <div>lectures</div>;
+  const Lecture = ({ lecture }) => {
+    console.log(lecture);
+    return (
+      <div>
+        {lecture.map((item) => {
+          return (
+            <>
+              <div>{item.title}</div>
+              <more>{item.text}</more>
+            </>
+          );
+        })}
+      </div>
+    );
+  };
+  const sections = ['test section', 'test section'];
+  const lecture = [
+    { title: 'test lecture', text: 'test' },
+    { title: 'test lecture', text: 'test' },
+  ];
+
   return (
     <div>
       <ul>
-        <li>Sections</li>
-        <Lecture />
+        {sections.map((value) => {
+          return (
+            <li>
+              {value}
+              <Lecture lecture={lecture} />
+            </li>
+          );
+        })}
+        {/* {Object.keys(data).forEach((key) => {
+          return <li value={key}>{data[key]}</li>;
+        })} */}
+        {/* {Object.entries(data).map(([key, value]) => (
+          <div key={key}>{value}</div>
+        ))} */}
       </ul>
     </div>
   );
