deaccba180cf002cfe5f5962a9ba09bf660d479c
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-20 00:45:42 +0530

    5 files modified

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './style.css';
 import { Header, Footer, Topics } from './components/index';
 import Home from './pages/Home';
+import Course from './pages/Course';
 import { Switch, Route, useParams, useRouteMatch } from 'react-router-dom';
 export default function App() {
   return (
     <div>
       <Header />
       <Switch>
+        <Route path="/course/course" exact>
+          <Course />
+        </Route>
         <Route path={`/course/:topicId`} children={<Topics />}></Route>
         <Route path="/*" exact>
           <Home />
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,4 +1,23 @@
 import React from 'react';
-export default function Course() {
+export default function Course({
+  title,
+  text,
+  preview_url,
+  rating,
+  author,
+  students,
+  last_updated,
+  language,
+}) {
+  const course = {
+    title: 'Create Personal Brand Niche Website without coding',
+    text: 'Using FREE Resources - Make Professional Personal Brand Website With Wordpress & Elementor',
+    preview_url: '',
+    rating: 5,
+    author: 'manjunath kalburgi',
+    students: 10,
+    last_updated: '2021-10-20',
+    language: 'english',
+  };
   return <div>Course</div>;
 }
