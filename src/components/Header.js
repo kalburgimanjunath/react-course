ec4ea441c9ad70efc89ea2ca259a6e03e4426b85
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-30 13:34:08 +0530

    1 file modified, 6 added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div>
-      <ul>
+      {/* <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -25,7 +25,90 @@ export default function Header() {
         <li>
           <Link to="/chat">Chat</Link>
         </li>
-      </ul>
+      </ul> */}
+      <header id="header" className="fixed-top">
+        <div className="container d-flex align-items-center">
+          <h1 className="logo me-auto">
+            <a href="index.html">Mentor</a>
+          </h1>
+
+          <nav id="navbar" className="navbar order-last order-lg-0">
+            <ul>
+              <li>
+                <a className="active" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="about.html">About</a>
+              </li>
+              <li>
+                <a href="courses.html">Courses</a>
+              </li>
+              <li>
+                <a href="trainers.html">Trainers</a>
+              </li>
+              <li>
+                <a href="events.html">Events</a>
+              </li>
+              <li>
+                <a href="pricing.html">Pricing</a>
+              </li>
+
+              <li className="dropdown">
+                <a href="#">
+                  <span>Drop Down</span> <i className="bi bi-chevron-down"></i>
+                </a>
+                <ul>
+                  <li>
+                    <a href="#">Drop Down 1</a>
+                  </li>
+                  <li className="dropdown">
+                    <a href="#">
+                      <span>Deep Drop Down</span>{' '}
+                      <i className="bi bi-chevron-right"></i>
+                    </a>
+                    <ul>
+                      <li>
+                        <a href="#">Deep Drop Down 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 2</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 3</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 4</a>
+                      </li>
+                      <li>
+                        <a href="#">Deep Drop Down 5</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Drop Down 4</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="contact.html">Contact</a>
+              </li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+
+          <a href="courses.html" className="get-started-btn">
+            Get Started
+          </a>
+        </div>
+      </header>
     </div>
   );
 }
